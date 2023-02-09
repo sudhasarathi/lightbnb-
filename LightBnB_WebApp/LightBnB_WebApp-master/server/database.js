da63506413613cf53c4b98a95197ba1f7ecfae6a
sudhasarathi <sudhav158@email.com>
2023-02-09 15:41:31 -0700

    updated users, getUserWithEmail, getUserWithId, addUser, getAllProperties

--- LightBnB_WebApp/LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp/LightBnB_WebApp-master/server/database.js
@@ -17,16 +17,18 @@ pool.connect ();
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  pool.query(`
+    SELECT * FROM users WHERE email = $1
+  `,[email]).then(resp => {
+    console.log(resp.rows)
+  });
+
+
+  return pool.query(`
+    SELECT * 
+    FROM users 
+    WHERE email = $1
+  `,[email]).then(resp => resp.rows[0]);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,8 +38,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  return pool.query(`
+  SELECT * 
+  FROM users
+  WHERE id = $1`,[id]).then(resp => resp.rows[0]);
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,11 +52,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool.query( `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `,[user.name, user.email, user.password]).then(resp => resp.rows[0]);
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -76,14 +82,12 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .query(`SELECT properties.*, avg(property_reviews.rating) AS average_rating
+    FROM properties
+    LEFT OUTER JOIN property_reviews ON property_id = properties.id
+    GROUP BY properties.id
+    LIMIT $1
+    `, [limit]).then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
